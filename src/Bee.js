e27e10184f4e7ea89259c1ee5909d6476410ac36
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-31 10:35:56 -0700

    completed beesbeesbees exercise

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
-};
+  //call grub before setting new properties
+  Grub.call(this);
 
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
+
+};
+//create relationship between Bee and Grub
+Bee.prototype = Object.create(Grub.prototype);
+//initialize constructor that points back at constructor
+Bee.prototype.constructor = Bee;
